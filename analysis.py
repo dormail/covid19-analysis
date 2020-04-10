34a97dbfb77785137bcc3643303dfc6690f7c8a6
dormail <59531240+dormail@users.noreply.github.com>
2020-04-10 13:35:23 +0200

    Add files via upload
    initial commit

---
+++ analysis.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+from element_of import element_of
+
+# loading source data from excel sheet in pandas DataFrame
+source = pd.read_excel("COVID-19-geographic-disbtribution-worldwide-2020-04-09.xlsx")
