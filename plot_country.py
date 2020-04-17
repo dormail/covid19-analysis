097c111ee46f6be0f984af955c92ccffc8833312
Matthias Maile <matthias.maile1@gmail.com>
2020-04-17 10:24:40 +0200

    renamed variable for future features

--- plot_country.py
+++ plot_country.py
@@ -10,24 +10,29 @@ def plot_one(country,data):
         raise TypeError
 
     # extracting info about spcific country
-    inf_temp = data[2].loc[data[2]["country/region"] == country]
-    deaths_temp = data[3].loc[data[2]["country/region"] == country]
+    inf_comul_pd = data[2].loc[data[2]["country/region"] == country]
+    inf_change_pd = data[0].loc[data[2]["country/region"] == country]
+    deaths_comul_pd = data[3].loc[data[2]["country/region"] == country]
+    deaths_comul_pd = data[1].loc[data[2]["country/region"] == country]
+
 
     # loading country info into list
-    infected = []
-    deaths = []
-    for i in range(1,inf_temp.shape[1]):
-        infected.append(inf_temp.iat[0,i])
-        deaths.append(deaths_temp.iat[0,i])
+    inf_comul, inf_change, deaths_comul, deaths_change = [], [], [], []
+    for i in range(1,inf_comul_pd.shape[1]):
+        inf_comul.append(inf_comul_pd.iat[0,i])
+        deaths_comul.append(deaths_comul_pd.iat[0,i])
+
+        #deaths.append(deaths_temp.iat[0,i])
+
 
 
     # data cleaning
     # removing initial spots that are smaller than minimum (to make graph more readable)
     minimum =  1000
-    for i in range(0,len(infected)):
-        if infected[0] < minimum:
-            infected.pop(0)
-            deaths.pop(0)
+    for i in range(0,len(inf_comul)):
+        if inf_comul[0] < minimum:
+            inf_comul.pop(0)
+            deaths_comul.pop(0)
         else:
             break
 
@@ -39,11 +44,11 @@ def plot_one(country,data):
     ax1.set_ylabel("no. of cases")
     ax1.set_title("Confirmed cases of Covid-19 in " + country)
 
-    x = np.linspace(0,len(infected),len(infected))
-    zeros = np.zeros(len(infected))
+    x = np.linspace(0,len(inf_comul),len(inf_comul))
+    zeros = np.zeros(len(inf_comul))
     # filled area:
-    ax1.fill_between(x,infected,zeros, color='tomato')
-    ax1.fill_between(x,deaths,zeros, color='black')
+    ax1.fill_between(x,inf_comul,zeros, color='tomato')
+    ax1.fill_between(x,deaths_comul,zeros, color='black')
 
 
     # normal plotting:
